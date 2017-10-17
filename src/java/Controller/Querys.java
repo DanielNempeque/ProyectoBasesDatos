3d1df99bbfec76d1673fb3fc8d36a6a3dda05231
DanielNempeque <daniel.nempeque@gmail.com>
2017-10-17 13:38:25 -0500

    Adding Auth

--- src/java/Controller/Querys.java
+++ src/java/Controller/Querys.java
@@ -5,8 +5,10 @@
  */
 package Controller;
 
-import com.mysql.jdbc.PreparedStatement;
+
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,9 +19,44 @@ public class Querys extends Conexion{
         PreparedStatement pst = null;
         ResultSet rs = null;
         try {
-            String consulta = "SELECT C.id_login, L.Usuario, L.Correo FROM login L";
+            String consulta = "SELECT * FROM login "
+                    + "WHERE (Usuario = ? "
+                    + "AND Password = ?)"
+                    + "OR (Correo = ?"
+                    + "AND Password = ?)";
+            pst = getConnection().prepareStatement(consulta);
+            pst.setString(1, user);
+            pst.setString(2, password);
+            pst.setString(3, user);
+            pst.setString(4, password);
+            rs = pst.executeQuery();
+            
+            if(rs.absolute(1)){
+                return true;
+            }
+            
         } catch (Exception e) {
+            System.err.println("ERROR: "+e);
+        }finally{
+            try {
+                if(getConnection() != null ) getConnection().close();
+                if(pst != null ) pst.close();
+                if(rs != null ) rs.close();
+            } catch (Exception e) {
+                System.err.println("ERROR: "+e);
+            }  
         }
         return false;
+    }
+    public static void main(String[] args) {
+        Querys que = new Querys();
+        String usr = JOptionPane.showInputDialog("Usuario");
+        String psw = JOptionPane.showInputDialog("Ingrese Password");
+        if(que.auth(usr, psw)){
+            System.out.println("Existe el usuario");
+        }else{
+            System.out.println("No existe el usuario");
+        }
+        
     }
 }
