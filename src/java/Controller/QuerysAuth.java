efb5813368cccb581590d16f1f1d8c466fa7ffa8
DanielNempeque <daniel.nempeque@gmail.com>
2017-10-17 17:19:43 -0500

    KUERIS ARE GUT :v

--- src/java/Controller/QuerysAuth.java
+++ src/java/Controller/QuerysAuth.java
@@ -14,7 +14,7 @@ import javax.swing.JOptionPane;
  *
  * @author Daniel Nempeque
  */
-public class Querys extends Conexion{
+public class QuerysAuth extends Conexion{
     public boolean auth(String user, String password){
         PreparedStatement pst = null;
         ResultSet rs = null;
@@ -48,8 +48,50 @@ public class Querys extends Conexion{
         }
         return false;
     }
+    public boolean register(String user,String correo, String password, String nombre, String telefono,String noDocumento, String fechaNacimiento ,String Genenro){
+        PreparedStatement pst = null;
+        PreparedStatement pst2 = null;
+        ResultSet rs = null;
+        ResultSet rs2 = null;
+        
+        try {
+            String register = "INSERT (Usuario,Password,Correo) INTO login VALUES (?,?,?)";
+            String cliente = "INSERT (Nombre,Telefono,Correo,No_Documento,Fecha_nacimiento,Genero) INTO login VALUES (?,?,?,?,?)";
+            pst = getConnection().prepareStatement(register);
+            pst2 = getConnection().prepareStatement(cliente);
+            pst.setString(1, user);
+            pst.setString(2, password);
+            pst.setString(3, correo);
+            pst2.setString(1, nombre);
+            pst2.setString(2, telefono);
+            pst2.setString(3, correo);
+            pst2.setString(4, noDocumento);
+            pst2.setString(5, fechaNacimiento);
+            pst2.setString(6, Genenro);
+            
+            rs = pst.executeQuery();
+            rs2 = pst2.executeQuery();
+            
+            return true;
+            
+            
+        } catch (Exception e) {
+            System.err.println("ERROR :" + e );
+        }finally{
+            try {
+                if(getConnection() != null ) getConnection().close();
+                if(pst != null ) pst.close();
+                if(rs != null ) rs.close();
+                if(pst2 != null ) pst2.close();
+                if(rs2 != null ) rs2.close();
+            } catch (Exception e) {
+                System.err.println("ERROR: "+e);
+            }  
+        }
+        return false;        
+    }
     public static void main(String[] args) {
-        Querys que = new Querys();
+        QuerysAuth que = new QuerysAuth();
         String usr = JOptionPane.showInputDialog("Usuario");
         String psw = JOptionPane.showInputDialog("Ingrese Password");
         if(que.auth(usr, psw)){
